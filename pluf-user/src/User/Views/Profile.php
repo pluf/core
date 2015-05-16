d39f03a4ba3d9c43ee0529d6d5158c0abad60a54
mostafa barmshory <mostafa.barmshory@dpq.co.ir>
2015-05-16 19:32:21 +0430

    ویرایش و نمایش پروفایلها به روز شده است.

--- pluf-user/src/User/Views/Profile.php
+++ pluf-user/src/User/Views/Profile.php
@@ -27,18 +27,34 @@ class User_Views_Profile {
 	 */
 	public function profile($request, $match) {
 		$profile_model = Pluf::f ( 'user_profile_class', false );
-		if($profile_model === false){
-			throw new Pluf_Exception(__('Profile model is not configured.'));
+		if ($profile_model === false) {
+			throw new Pluf_Exception ( __ ( 'Profile model is not configured.' ) );
 		}
 		try {
-			$profile = $request->user->getProfile();
+			$profile = $request->user->getProfile ();
 		} catch ( Pluf_Exception_DoesNotExist $ex ) {
 			$profile = new $profile_model ();
 			$profile->user = $request->user;
 			$profile->create ();
 		}
-		// Return response
-		return new Pluf_HTTP_Response_Json ( $profile );
+		
+		if ($request->method == 'GET') {
+			// Return response
+			return new Pluf_HTTP_Response_Json ( $profile );
+		}
+		
+		if ($request->method == 'POST') {
+			$profile_form = Pluf::f ( 'user_profile_form', false );
+			if ($profile_form === false) {
+				throw new Pluf_Exception ( __ ( 'Profile form is not configured.' ) );
+			}
+			$form = new $profile_form ( array_merge ( $request->POST, $request->FILES ), array (
+					'user_profile' => $profile 
+			) );
+			$profile = $form->update ();
+			return new Pluf_HTTP_Response_Json ( $profile );
+		}
+		
+		throw  new Pluf_Exception("Curent REST method is not supported");
 	}
-
 }
