f1e1e3d2466eff7a8494aa6bc186864ef05f252b
hadi <mohammad.hadi.mansouri@chmail.ir>
2015-09-29 10:54:25 +0330

    کلاس PPaginatorPage اضافه شد

---
+++ retrofit-pluf/src/main/java/ir/co/dpq/pluf/PPaginatorPage.java
@@ -0,0 +1,26 @@
+package ir.co.dpq.pluf;
+
+import java.util.List;
+
+public class PPaginatorPage<T> {
+
+	public List<T> getList() {
+		return null;
+	}
+
+	public int getPageIndex() {
+		return 0;
+	}
+
+	public int getPageCount() {
+		return 0;
+	}
+
+	public int getItemPerPage() {
+		return 0;
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+}
