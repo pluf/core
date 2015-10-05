fb375047aedd2d0fce9ff91c31ae88b2a49bf0fb
hadi <mohammad.hadi.mansouri@chmail.ir>
2015-10-05 21:40:39 +0330

    خطای تبدیل Location به ... برطرف شد

--- retrofit-pluf/src/main/java/ir/co/dpq/pluf/DeserializerJson.java
+++ retrofit-pluf/src/main/java/ir/co/dpq/pluf/DeserializerJson.java
@@ -1,5 +1,6 @@
 package ir.co.dpq.pluf;
 
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +26,14 @@ public class DeserializerJson<T> implements JsonDeserializer<PPaginatorPage<T>> 
         Type paginatorPageType = new TypeToken<PPaginatorPage<T>>() {}.getType();
         PPaginatorPage<T> pagedResponse = gson.fromJson(je, paginatorPageType);
 
-        Type ct = new TypeToken<T>() {}.getType();
+        
+        Type ct ;
+        ct = new TypeToken<T>() {}.getType();
+        if(type instanceof ParameterizedType){
+        	ParameterizedType pt = (ParameterizedType) type;
+        	if(pt.getActualTypeArguments().length > 0)
+        	ct = pt.getActualTypeArguments()[0];
+        }
         JsonElement jeI = je.getAsJsonObject().get("items");
         List<T> items = new ArrayList<T>();
         Set<Entry<String, JsonElement>> set = jeI.getAsJsonObject().entrySet();
