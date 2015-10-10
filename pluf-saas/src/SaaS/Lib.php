cf5db49a9184402ce5e15bb232b49f78296509a1
mostafa barmshory <mostafa.barmshory@dpq.co.ir>
2015-10-10 18:46:07 +0330

    یک سری ساختار داده‌ای جدید اضافه شده که بتونیم استفاده از نرم افزارها رو مدیریت کنیم.

---
+++ pluf-saas/src/SaaS/Lib.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * 
+ * @author maso
+ *
+ */
+class SaaS_SAP extends Pluf_Model
+{
+
+    /**
+     * (non-PHPdoc)
+     * @see Pluf_Model::init()
+     */
+    function init ()
+    {
+        $this->_model = 'SaaS_SAP';
+        $this->_a['table'] = 'saas_sap';
+        $this->_a['model'] = $this->_model;
+        $this->_a['cols'] = array(
+                'id' => array(
+                        'type' => 'Pluf_DB_Field_Sequence',
+                        'blank' => true
+                ),
+                'mode' => array(
+                        'type' => 'Pluf_DB_Field_Integer',
+                        'blank' => false,
+                        'unique' => false
+                ),
+                'type' => array(
+                        'type' => 'Pluf_DB_Field_Integer',
+                        'blank' => false,
+                        'unique' => false
+                ),
+                'name' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => true,
+                        'size' => 100
+                ),
+                'description' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => true,
+                        'size' => 250
+                ),
+                'creation_dtime' => array(
+                        'type' => 'Pluf_DB_Field_Datetime',
+                        'blank' => true
+                ),
+                'modif_dtime' => array(
+                        'type' => 'Pluf_DB_Field_Datetime',
+                        'blank' => true
+                )
+        );
+    }
+
+    /**
+     * \brief پیش ذخیره را انجام می‌دهد
+     *
+     * @param $create حالت
+     *            ساخت یا به روز رسانی را تعیین می‌کند
+     */
+    function preSave ($create = false)
+    {
+        if ($this->id == '') {
+            $this->creation_dtime = gmdate('Y-m-d H:i:s');
+        }
+        $this->modif_dtime = gmdate('Y-m-d H:i:s');
+    }
+
+    function preDelete ()
+    {
+        // @unlink(Pluf::f('upload_issue_path').'/'.$this->attachment);
+    }
+
+    /**
+     * حالت کار ایجاد شده را به روز می‌کند
+     *
+     * @see Pluf_Model::postSave()
+     */
+    function postSave ($create = false)
+    {
+        //
+    }
+}
---
+++ pluf-saas/src/SaaS/SAP.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * 
+ * @author maso
+ *
+ */
+class SaaS_SAP extends Pluf_Model
+{
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see Pluf_Model::init()
+     */
+    function init ()
+    {
+        $this->_model = 'SaaS_SAP';
+        $this->_a['table'] = 'saas_sap';
+        $this->_a['model'] = $this->_model;
+        $this->_a['cols'] = array(
+                'id' => array(
+                        'type' => 'Pluf_DB_Field_Sequence',
+                        'blank' => true
+                ),
+                'title' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => true,
+                        'size' => 100
+                ),
+                'description' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => true,
+                        'size' => 250
+                ),
+                'path' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => false,
+                        'size' => 100
+                ),
+                'creation_dtime' => array(
+                        'type' => 'Pluf_DB_Field_Datetime',
+                        'blank' => true
+                ),
+                'modif_dtime' => array(
+                        'type' => 'Pluf_DB_Field_Datetime',
+                        'blank' => true
+                )
+        );
+    }
+
+    /**
+     * پیش ذخیره را انجام می‌دهد
+     *
+     * @param $create حالت
+     *            ساخت یا به روز رسانی را تعیین می‌کند
+     */
+    function preSave ($create = false)
+    {
+        if ($this->id == '') {
+            $this->creation_dtime = gmdate('Y-m-d H:i:s');
+        }
+        $this->modif_dtime = gmdate('Y-m-d H:i:s');
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see Pluf_Model::preDelete()
+     */
+    function preDelete ()
+    {
+        // @unlink(Pluf::f('upload_issue_path').'/'.$this->attachment);
+    }
+
+    /**
+     * حالت کار ایجاد شده را به روز می‌کند
+     *
+     * @see Pluf_Model::postSave()
+     */
+    function postSave ($create = false)
+    {
+        //
+    }
+}
