0453cec45a510efa881818e79ae402ea9f497586
mostafa barmshory <mostafa.barmshory@dpq.co.ir>
2015-11-15 23:47:53 +0330

    یه سوتی

--- jpluf/jpluf-retrofit/src/main/java/ir/co/dpq/pluf/PProfileAdministratorRetrofit.java
+++ jpluf/jpluf-retrofit/src/main/java/ir/co/dpq/pluf/PProfileAdministratorRetrofit.java
@@ -1,7 +1,6 @@
 package ir.co.dpq.pluf;
 
 import ir.co.dpq.pluf.IPCallback;
-import ir.co.dpq.pluf.retrofit.CallbackWraper;
 import ir.co.dpq.pluf.retrofit.Util;
 import ir.co.dpq.pluf.retrofit.user.IRProfileAdministrator;
 import ir.co.dpq.pluf.retrofit.user.RProfile;
@@ -11,11 +10,23 @@ import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
+/**
+ * 
+ * @author maso
+ *
+ */
 public class PProfileAdministratorRetrofit implements IPProfileAdministrator {
 
 	// @Inject
 	IRProfileAdministrator profileAdministrator;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ir.co.dpq.pluf.user.IPProfileAdministrator#getProfile(java.lang.Long,
+	 * ir.co.dpq.pluf.IPCallback)
+	 */
 	@Override
 	public void getProfile(Long id, final IPCallback<PProfile> callback) {
 		profileAdministrator.getProfile(id, new Callback<RProfile>() {
@@ -34,15 +45,28 @@ public class PProfileAdministratorRetrofit implements IPProfileAdministrator {
 		});
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ir.co.dpq.pluf.user.IPProfileAdministrator#getProfile(java.lang.Long)
+	 */
 	@Override
 	public PProfile getProfile(Long id) {
 		return profileAdministrator.getProfile(id);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ir.co.dpq.pluf.user.IPProfileAdministrator#updateProfile(ir.co.dpq.pluf.
+	 * user.PProfile, ir.co.dpq.pluf.IPCallback)
+	 */
 	@Override
 	public void updateProfile(PProfile profile, final IPCallback<PProfile> callback) {
 		RProfile rprofile = Util.toRObject(profile);
-		profileAdministrator.updateProfile(rprofile.getId(), rprofile.toMap(),  new Callback<RProfile>() {
+		profileAdministrator.updateProfile(rprofile.getId(), rprofile.toMap(), new Callback<RProfile>() {
 
 			@Override
 			public void success(RProfile t, Response response) {
@@ -58,6 +82,13 @@ public class PProfileAdministratorRetrofit implements IPProfileAdministrator {
 		});
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * ir.co.dpq.pluf.user.IPProfileAdministrator#updateProfile(ir.co.dpq.pluf.
+	 * user.PProfile)
+	 */
 	@Override
 	public PProfile updateProfile(PProfile profile) {
 		RProfile rprofile = Util.toRObject(profile);
