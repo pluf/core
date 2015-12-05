13d8ae80e72ad8699946f1d1d6ba6b148d4df83e
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2015-12-05 13:08:48 +0330

    زمانی که نمایش اصلی خالی باشه یک پیام خطا در خروجی چاپ می‌شود.

--- pluf/src/SaaS/Template/SapMainView.php
+++ pluf/src/SaaS/Template/SapMainView.php
@@ -19,6 +19,10 @@ class SaaS_Template_SapMainView extends Pluf_Template_Tag
     {
         if (is_readable($filename)) {
             $myfile = fopen($filename, "r") or die("Unable to open file!");
+            if(filesize($filename) == 0){
+                echo "Empty view!?";
+                return;
+            }
             $content = fread($myfile, filesize($filename));
             fclose($myfile);
             echo $content;
