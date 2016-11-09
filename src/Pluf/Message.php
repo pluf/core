30ae639ae36b0bb42797603a80dba95a4a26c94e
hadi <mohammad.hadi.mansouri@chmail.ir>
2016-11-09 13:44:08 +0330

    ویرایش جزیی در Pluf_Message

--- src/Pluf/Message.php
+++ src/Pluf/Message.php
@@ -15,62 +15,75 @@ class Pluf_Message extends Pluf_Model
 
     public $_model = 'Pluf_Message';
 
-    function init ()
+    function init()
     {
         $this->_a['table'] = 'messages';
         $this->_a['model'] = 'Pluf_Message';
         $this->_a['cols'] = array(
-                // It is mandatory to have an "id" column.
-                'id' => array(
-                        'type' => 'Pluf_DB_Field_Sequence',
-                        // It is automatically added.
-                        'blank' => true,
-                        'editable' => false,
-                        'readable' => true
-                ),
-                'version' => array(
-                        'type' => 'Pluf_DB_Field_Integer',
-                        'blank' => true,
-                        'editable' => false,
-                        'readable' => true
-                ),
-                'user' => array(
-                        'type' => 'Pluf_DB_Field_Foreignkey',
-                        'model' => Pluf::f('pluf_custom_user', 'Pluf_User'),
-                        'blank' => false,
-                        'verbose' => __('user'),
-                        'editable' => false,
-                        'readable' => false
-                ),
-                'message' => array(
-                        'type' => 'Pluf_DB_Field_Text',
-                        'blank' => false,
-                        'editable' => false,
-                        'readable' => true
-                ),
-                'creation_dtime' => array(
-                        'type' => 'Pluf_DB_Field_Datetime',
-                        'blank' => true,
-                        'editable' => false,
-                        'readable' => true
-                ),
-                'tenant' => array(
-                        'type' => 'Pluf_DB_Field_Integer',
-                        'blank' => false,
-                        'editable' => false,
-                        'readable' => false
-                )
+            // It is mandatory to have an "id" column.
+            'id' => array(
+                'type' => 'Pluf_DB_Field_Sequence',
+                // It is automatically added.
+                'blank' => true,
+                'editable' => false,
+                'readable' => true
+            ),
+            'version' => array(
+                'type' => 'Pluf_DB_Field_Integer',
+                'blank' => true,
+                'editable' => false,
+                'readable' => true
+            ),
+            'user' => array(
+                'type' => 'Pluf_DB_Field_Foreignkey',
+                'model' => Pluf::f('pluf_custom_user', 'Pluf_User'),
+                'blank' => false,
+                'verbose' => __('user'),
+                'editable' => false,
+                'readable' => false
+            ),
+            'message' => array(
+                'type' => 'Pluf_DB_Field_Text',
+                'blank' => false,
+                'editable' => false,
+                'readable' => true
+            ),
+            'creation_dtime' => array(
+                'type' => 'Pluf_DB_Field_Datetime',
+                'blank' => true,
+                'editable' => false,
+                'readable' => true
+            ),
+            'tenant' => array(
+                'type' => 'Pluf_DB_Field_Integer',
+                'blank' => false,
+                'editable' => false,
+                'readable' => false
+            )
         );
         $this->_a['idx'] = array(
-                'message_user_idx' => array(
-                        'type' => 'normal',
-                        'col' => 'tenant, user'
-                )
+            'message_user_idx' => array(
+                'type' => 'normal',
+                'col' => 'tenant, user'
+            )
         );
     }
 
-    function __toString ()
+    function __toString()
     {
         return $this->message;
+    }
+
+    /**
+     * پیش ذخیره را انجام می‌دهد
+     *
+     * @param $create حالت
+     *            ساخت یا به روز رسانی را تعیین می‌کند
+     */
+    function preSave($create = false)
+    {
+        if ($this->id == '') {
+            $this->creation_dtime = gmdate('Y-m-d H:i:s');
+        }
     }
 }
