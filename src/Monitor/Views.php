9cea91a5f80a776cc39448b5df0f5d1843e52cdc
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-02-04 02:30:51 +0330

    update monitors

--- src/Monitor/Views.php
+++ src/Monitor/Views.php
@@ -21,34 +21,7 @@ Pluf::loadFunction('Pluf_Shortcuts_GetObjectOr404');
 class Monitor_Views
 {
 
-    public static function create ($request, $match)
-    {
-        // initial content data
-        $extra = array(
-                'user' => $request->user,
-                'tenant' => $request->tenant,
-                'model' => new CMS_Content()
-        );
-        
-        // Create content and get its ID
-        $form = new CMS_Form_ContentCreate($request->REQUEST, $extra);
-        
-        // Upload content file and extract information about it (by updating
-        // content)
-        $extra['model'] = $form->save();
-        $form = new CMS_Form_ContentUpdate(
-                array_merge($request->REQUEST, $request->FILES), $extra);
-        try {
-            $content = $form->save();
-        } catch (Pluf_Exception $e) {
-            $content = $extra['model'];
-            $content->delete();
-            throw $e;
-        }
-        return new Pluf_HTTP_Response_Json($content);
-    }
-
-    public static function find ($request, $match)
+    public static function findBean ($request, $match)
     {
         $content = new Pluf_Paginator(new Pluf_Monitor());
         // $sql = new Pluf_SQL('tenant=%s',
@@ -56,6 +29,47 @@ class Monitor_Views
         // $request->tenant->id
         // ));
         // $content->forced_where = $sql;
+        $content->model_view = 'beans';
+        $content->list_filters = array(
+                'bean',
+                'property',
+                'title'
+        );
+        $list_display = array(
+                'title' => __('title'),
+                'bean' => __('bean name'),
+                'property' => __('property'),
+                'description' => __('description')
+        );
+        $search_fields = array(
+                'title',
+                'description',
+                'bean',
+                'property'
+        );
+        $sort_fields = array(
+                'id',
+                'name',
+                'title',
+                'bean',
+                'property',
+                'creation_date',
+                'modif_dtime'
+        );
+        $content->configure($list_display, $search_fields, $sort_fields);
+        $content->setFromRequest($request);
+        return new Pluf_HTTP_Response_Json($content->render_object());
+    }
+
+    public static function findProperty ($request, $match)
+    {
+        $content = new Pluf_Paginator(new Pluf_Monitor());
+        $sql = new Pluf_SQL('bean=%s', 
+                array(
+                        $match['monitor']
+                ));
+        $content->forced_where = $sql;
+        $content->model_view = 'properties';
         $content->list_filters = array(
                 'bean',
                 'property',
