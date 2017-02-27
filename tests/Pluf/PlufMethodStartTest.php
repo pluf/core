0a4cf53f2bc4fd96a42844c8fb1687c1bf2dd6d6
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-02-27 04:29:39 +0330

    Run pluf test.

--- tests/Pluf/PlufMethodStartTest.php
+++ tests/Pluf/PlufMethodStartTest.php
@@ -1,45 +1,42 @@
 <?php
-/* -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
-# ***** BEGIN LICENSE BLOCK *****
-# This file is part of Plume Framework, a simple PHP Application Framework.
-# Copyright (C) 2001-2007 Loic d'Anterroches and contributors.
-#
-# Plume Framework is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) any later version.
-#
-# Plume Framework is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# ***** END LICENSE BLOCK ***** */
-
-error_reporting(E_ALL | E_STRICT);
-
-$path = dirname(__FILE__).'/../../src/';
-set_include_path(get_include_path().PATH_SEPARATOR.$path);
-
-require_once 'PHPUnit/Framework/TestCase.php';
-require_once 'PHPUnit/Framework/IncompleteTestError.php';
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\IncompleteTestError;
 
 require_once 'Pluf.php';
 
-class PlufMethodStartTest extends PHPUnit_Framework_TestCase {
-    
-    protected function setUp()
-    {
-    }
+class PlufMethodStartTest extends TestCase
+{
 
-    public function testStart()
+    /**
+     * @before
+     */
+    public function setUp ()
+    {}
+
+    /**
+     *
+     * @test
+     */
+    public function testStart ()
     {
-        Pluf::start(dirname(__FILE__).'/../conf/pluf.config.php');
+        Pluf::start(dirname(__FILE__) . '/../conf/pluf.config.php');
         $this->assertEquals(true, isset($GLOBALS['_PX_config']));
         $this->assertEquals(false, $GLOBALS['_PX_config']['test']);
     }
