2d4f37d15a0eb6e62fc6a36aced5cc4453fc5aca
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-03-02 10:16:26 +0330

    Form tests

--- tests/Pluf_Form/PlufFormTest.php
+++ tests/Pluf_Form/PlufFormTest.php
@@ -1,44 +1,34 @@
 <?php
-/* -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
-# ***** BEGIN LICENSE BLOCK *****
-# This file is part of Plume Framework, a simple PHP Application Framework.
-# Copyright (C) 2001-2007 Loic d'Anterroches and contributors.
-#
-# Plume Framework is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) any later version.
-#
-# Plume Framework is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# ***** END LICENSE BLOCK ***** */
-
-error_reporting(E_ALL | E_STRICT);
-
-$path = dirname(__FILE__).'/../../src/';
-set_include_path(get_include_path().PATH_SEPARATOR.$path);
-
-require_once 'PHPUnit/Framework/TestCase.php';
-require_once 'PHPUnit/Framework/IncompleteTestError.php';
-
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\IncompleteTestError;
 require_once 'Pluf.php';
-require_once dirname(__FILE__).'/TestFormModel.php';
 
+require_once dirname(__FILE__) . '/TestFormModel.php';
 
-class PlufFormTest extends PHPUnit_Framework_TestCase {
-    
-    protected function setUp()
+class PlufFormTest extends TestCase
+{
+
+    protected function setUp ()
     {
         $this->markTestSkipped('Need to rewrite the form handling.');
-        Pluf::start(dirname(__FILE__).'/../conf/pluf.config.php');
+        Pluf::start(dirname(__FILE__) . '/../conf/pluf.config.php');
         $db = Pluf::db();
         $schema = Pluf::factory('Pluf_DB_Schema', $db);
         $m1 = new TestFormModel();
@@ -47,7 +37,7 @@ class PlufFormTest extends PHPUnit_Framework_TestCase {
         $schema->createTables();
     }
 
-    protected function tearDown()
+    protected function tearDown ()
     {
         $db = Pluf::db();
         $schema = Pluf::factory('Pluf_DB_Schema', $db);
@@ -56,7 +46,7 @@ class PlufFormTest extends PHPUnit_Framework_TestCase {
         $schema->dropTables();
     }
 
-    public function testFormRenderByField()
+    public function testFormRenderByField ()
     {
         $m1 = new TestFormModel();
         $form = new Pluf_Form_Create();
@@ -67,13 +57,14 @@ class PlufFormTest extends PHPUnit_Framework_TestCase {
         $m1->create();
         $new_data = $m1->getData();
         $form_view = new Pluf_Form_Render($form, $new_data, $errors);
-        $this->assertEquals('<label class="px-form-required" for="title">Title of the item:</label>',
-                            $form_view->fields['title']->label);
-        $this->assertEquals('<label for="description">Description:</label>',
-                            $form_view->fields['description']->label);
+        $this->assertEquals(
+                '<label class="px-form-required" for="title">Title of the item:</label>', 
+                $form_view->fields['title']->label);
+        $this->assertEquals('<label for="description">Description:</label>', 
+                $form_view->fields['description']->label);
     }
 
-    public function testFormValidate()
+    public function testFormValidate ()
     {
         $m1 = new TestFormModel();
         $form = new Pluf_Form_Update();
@@ -87,8 +78,6 @@ class PlufFormTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(true, isset($errors['id']));
         $this->assertEquals(1, count($errors));
     }
-
-
 }
 
 ?>
--- tests/Pluf_Form_Field/PlufFormFieldTest.php
+++ tests/Pluf_Form_Field/PlufFormFieldTest.php
@@ -1,59 +1,55 @@
 <?php
-/* -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
-# ***** BEGIN LICENSE BLOCK *****
-# This file is part of Plume Framework, a simple PHP Application Framework.
-# Copyright (C) 2001-2007 Loic d'Anterroches and contributors.
-#
-# Plume Framework is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2.1 of the License, or
-# (at your option) any later version.
-#
-# Plume Framework is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# ***** END LICENSE BLOCK ***** */
-
-error_reporting(E_ALL | E_STRICT);
-
-$path = dirname(__FILE__).'/../../src/';
-set_include_path(get_include_path().PATH_SEPARATOR.$path);
-
-require_once 'PHPUnit/Framework/TestCase.php';
-require_once 'PHPUnit/Framework/IncompleteTestError.php';
-
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\IncompleteTestError;
 require_once 'Pluf.php';
 
-class PlufFormFieldTest extends PHPUnit_Framework_TestCase {
-    
-    protected function setUp()
+class PlufFormFieldTest extends TestCase
+{
+
+    protected function setUp ()
     {
-        Pluf::start(dirname(__FILE__).'/../conf/pluf.config.php');
+        Pluf::start(dirname(__FILE__) . '/../conf/pluf.config.php');
     }
 
-    public function testPlufFormFieldFloat()
+    public function testPlufFormFieldFloat ()
     {
         $field = new Pluf_Form_Field_Float();
-        $valids = array(1234.12,
-                        123.0,
-                        123e+32);
-        $invalids = array('12,34.12',
-                        '12+3.0',
-                        '123es+32');
+        $valids = array(
+                1234.12,
+                123.0,
+                123e+32
+        );
+        $invalids = array(
+                '12,34.12',
+                '12+3.0',
+                '123es+32'
+        );
         foreach ($valids as $valid) {
             $this->assertEquals($valid, $field->clean($valid));
         }
         foreach ($invalids as $invalid) {
             try {
                 $field->clean($invalid);
-                $this->fail('An expected Exception has not been raised. Not a valid float: '.$invalid);
+                $this->fail(
+                        'An expected Exception has not been raised. Not a valid float: ' .
+                                 $invalid);
             } catch (Pluf_Form_Invalid $expected) {
                 continue;
             }
@@ -61,43 +57,45 @@ class PlufFormFieldTest extends PHPUnit_Framework_TestCase {
         return;
     }
 
-    public function testPlufFormFieldEmail()
+    public function testPlufFormFieldEmail ()
     {
         $field = new Pluf_Form_Field_Email();
         $valids = array(
-                        'cal@iamcalx.com',
-                        'cal+henderson@iamcalx.com',
-                        '"cal henderson"@iamcalx.com',
-                        'cal@iamcalx',
-                        'cal@[hello.com]',
-                        'cal@[hello world.com]',
-                        'cal@[hello\\ world.com]',
-                        'abcdefghijklmnopqrstuvwxyz@abcdefghijklmnopqrstuvwxyz',
-                        '"woo yay"@example.com',
-                        '"woo@yay"@example.com',
-                        '"woo.yay"@example.com',
-                        '"woo\\"yay"@test.com',
-                        'toto+yop@gmail.com',
-                        'webstaff@redcross.org',
-                        );
+                'cal@iamcalx.com',
+                'cal+henderson@iamcalx.com',
+                '"cal henderson"@iamcalx.com',
+                'cal@iamcalx',
+                'cal@[hello.com]',
+                'cal@[hello world.com]',
+                'cal@[hello\\ world.com]',
+                'abcdefghijklmnopqrstuvwxyz@abcdefghijklmnopqrstuvwxyz',
+                '"woo yay"@example.com',
+                '"woo@yay"@example.com',
+                '"woo.yay"@example.com',
+                '"woo\\"yay"@test.com',
+                'toto+yop@gmail.com',
+                'webstaff@redcross.org'
+        );
         $invalids = array(
-                        'cal henderson@iamcalx.com',
-                        'cal@iamcalx com',
-                        'cal@hello world.com',
-                        'cal@[hello].com',
-                        'cal@[hello world].com',
-                        'cal@[hello\\ world].com',
-                        'woo\\ yay@example.com',
-                        'woo\\@yay@example.com',
-                        'woo\\.yay@example.com',
-                          );
+                'cal henderson@iamcalx.com',
+                'cal@iamcalx com',
+                'cal@hello world.com',
+                'cal@[hello].com',
+                'cal@[hello world].com',
+                'cal@[hello\\ world].com',
+                'woo\\ yay@example.com',
+                'woo\\@yay@example.com',
+                'woo\\.yay@example.com'
+        );
         foreach ($valids as $valid) {
             $this->assertEquals($valid, $field->clean($valid));
         }
         foreach ($invalids as $invalid) {
             try {
                 $field->clean($invalid);
-                $this->fail('An expected Exception has not been raised. Not a valid email: '.$invalid);
+                $this->fail(
+                        'An expected Exception has not been raised. Not a valid email: ' .
+                                 $invalid);
             } catch (Pluf_Form_Invalid $expected) {
                 continue;
             }
