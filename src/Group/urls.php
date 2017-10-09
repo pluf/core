cac93e7270142685797a481ede7ac1de8486fef4
hadi <hadi@dpq.co.ir>
2017-10-10 00:09:08 +0330

    complete rests related to groups

--- src/Group/urls.php
+++ src/Group/urls.php
@@ -17,150 +17,150 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 return array(
-        // --------------------------------------------------------------
-        // Groups
-        // --------------------------------------------------------------
-        array(
-                'regex' => '#^/new$#',
-                'model' => 'Pluf_Views',
-                'method' => 'createObject',
-                'http-method' => 'POST',
-                'precond' => array(
-                        'Pluf_Precondition::ownerRequired'
-                ),
-                'params' => array(
-                        'model' => 'Pluf_Group'
-                )
+    // --------------------------------------------------------------
+    // Groups
+    // --------------------------------------------------------------
+    array(
+        'regex' => '#^/new$#',
+        'model' => 'Pluf_Views',
+        'method' => 'createObject',
+        'http-method' => 'POST',
+        'precond' => array(
+            'Pluf_Precondition::ownerRequired'
         ),
-        array(
-                'regex' => '#^/find$#',
-                'model' => 'Pluf_Views',
-                'method' => 'findObject',
-                'http-method' => 'GET',
-                'precond' => array(),
-                'params' => array(
-                        'model' => 'Pluf_Group',
-                        'listFilters' => array(
-                                'name',
-                                'description'
-                        ),
-                        'listDisplay' => array(
-                                'name' => 'name',
-                                'description' => 'description'
-                        ),
-                        'searchFields' => array(
-                                'name',
-                                'description'
-                        ),
-                        'sortFields' => array(
-                                'name',
-                                'description'
-                        ),
-                        'sortOrder' => array(
-                                'id',
-                                'DESC'
-                        )
-                )
-        ),
-        array(
-                'regex' => '#^/(?P<modelId>\d+)$#',
-                'model' => 'Pluf_Views',
-                'method' => 'getObject',
-                'http-method' => 'GET',
-                'params' => array(
-                        'model' => 'Pluf_Group'
-                ),
-                'precond' => array()
-        ),
-        array(
-                'regex' => '#^/(?P<modelId>\d+)$#',
-                'model' => 'Pluf_Views',
-                'method' => 'updateObject',
-                'http-method' => 'POST',
-                'params' => array(
-                        'model' => 'Pluf_Group'
-                ),
-                'precond' => array(
-                        'Pluf_Precondition::ownerRequired'
-                )
-        ),
-        array(
-                'regex' => '#^/(?P<modelId>\d+)$#',
-                'model' => 'Pluf_Views',
-                'method' => 'deleteObject',
-                'http-method' => 'DELETE',
-                'params' => array(
-                        'model' => 'Pluf_Group'
-                ),
-                'precond' => array(
-                        'Pluf_Precondition::ownerRequired'
-                )
-        ),
-        
-        // --------------------------------------------------------------
-        // Group rolses
-        // --------------------------------------------------------------
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/role$#',
-                'model' => 'Group_Views_Role',
-                'method' => 'add',
-                'http-method' => 'POST',
-                'precond' => array(
-                        'Pluf_Precondition::ownerRequired'
-                )
-        ),
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/role/find$#',
-                'model' => 'Group_Views_Role',
-                'method' => 'find',
-                'http-method' => 'GET'
-        ),
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/role/(?P<roleId>\d+)$#',
-                'model' => 'Group_Views_Role',
-                'method' => 'get',
-                'http-method' => 'GET'
-        ),
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/role/(?P<roleId>\d+)$#',
-                'model' => 'Group_Views_Role',
-                'method' => 'delete',
-                'http-method' => 'DELETE',
-                'precond' => array(
-                        'Pluf_Precondition::ownerRequired'
-                )
-        ),
-        // --------------------------------------------------------------
-        // Group users
-        // --------------------------------------------------------------
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/user$#',
-                'model' => 'Group_Views_User',
-                'method' => 'add',
-                'http-method' => 'POST',
-                'precond' => array(
-                        'Pluf_Precondition::ownerRequired'
-                )
-        ),
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/user/find$#',
-                'model' => 'Group_Views_User',
-                'method' => 'find',
-                'http-method' => 'GET'
-        ),
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/user/(?P<userId>\d+)$#',
-                'model' => 'Group_Views_User',
-                'method' => 'get',
-                'http-method' => 'GET'
-        ),
-        array(
-                'regex' => '#^/(?P<groupId>\d+)/user/(?P<userId>\d+)$#',
-                'model' => 'Group_Views_User',
-                'method' => 'delete',
-                'http-method' => 'DELETE',
-                'precond' => array(
-                        'Pluf_Precondition::loginRequired'
-                )
+        'params' => array(
+            'model' => 'Pluf_Group'
         )
+    ),
+    array(
+        'regex' => '#^/find$#',
+        'model' => 'Pluf_Views',
+        'method' => 'findObject',
+        'http-method' => 'GET',
+        'precond' => array(),
+        'params' => array(
+            'model' => 'Pluf_Group',
+            'listFilters' => array(
+                'name',
+                'description'
+            ),
+            'listDisplay' => array(
+                'name' => 'name',
+                'description' => 'description'
+            ),
+            'searchFields' => array(
+                'name',
+                'description'
+            ),
+            'sortFields' => array(
+                'name',
+                'description'
+            ),
+            'sortOrder' => array(
+                'id',
+                'DESC'
+            )
+        )
+    ),
+    array(
+        'regex' => '#^/(?P<modelId>\d+)$#',
+        'model' => 'Pluf_Views',
+        'method' => 'getObject',
+        'http-method' => 'GET',
+        'params' => array(
+            'model' => 'Pluf_Group'
+        ),
+        'precond' => array()
+    ),
+    array(
+        'regex' => '#^/(?P<modelId>\d+)$#',
+        'model' => 'Pluf_Views',
+        'method' => 'updateObject',
+        'http-method' => 'POST',
+        'params' => array(
+            'model' => 'Pluf_Group'
+        ),
+        'precond' => array(
+            'Pluf_Precondition::ownerRequired'
+        )
+    ),
+    array(
+        'regex' => '#^/(?P<modelId>\d+)$#',
+        'model' => 'Pluf_Views',
+        'method' => 'deleteObject',
+        'http-method' => 'DELETE',
+        'params' => array(
+            'model' => 'Pluf_Group'
+        ),
+        'precond' => array(
+            'Pluf_Precondition::ownerRequired'
+        )
+    ),
+    
+    // --------------------------------------------------------------
+    // Group rolses
+    // --------------------------------------------------------------
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/role$#',
+        'model' => 'Group_Views_Role',
+        'method' => 'add',
+        'http-method' => 'POST',
+        'precond' => array(
+            'Pluf_Precondition::ownerRequired'
+        )
+    ),
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/role/find$#',
+        'model' => 'Group_Views_Role',
+        'method' => 'find',
+        'http-method' => 'GET'
+    ),
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/role/(?P<roleId>\d+)$#',
+        'model' => 'Group_Views_Role',
+        'method' => 'get',
+        'http-method' => 'GET'
+    ),
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/role/(?P<roleId>\d+)$#',
+        'model' => 'Group_Views_Role',
+        'method' => 'delete',
+        'http-method' => 'DELETE',
+        'precond' => array(
+            'Pluf_Precondition::ownerRequired'
+        )
+    ),
+    // --------------------------------------------------------------
+    // Group users
+    // --------------------------------------------------------------
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/user$#',
+        'model' => 'Group_Views_User',
+        'method' => 'add',
+        'http-method' => 'POST',
+        'precond' => array(
+            'Pluf_Precondition::ownerRequired'
+        )
+    ),
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/user/find$#',
+        'model' => 'Group_Views_User',
+        'method' => 'find',
+        'http-method' => 'GET'
+    ),
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/user/(?P<userId>\d+)$#',
+        'model' => 'Group_Views_User',
+        'method' => 'get',
+        'http-method' => 'GET'
+    ),
+    array(
+        'regex' => '#^/(?P<groupId>\d+)/user/(?P<userId>\d+)$#',
+        'model' => 'Group_Views_User',
+        'method' => 'delete',
+        'http-method' => 'DELETE',
+        'precond' => array(
+            'Pluf_Precondition::loginRequired'
+        )
+    )
 );
