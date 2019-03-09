2f09ca54af12da568a11c78205bb63e5ee199ec3
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2019-03-09 08:44:37 +0330

    Update exception code

--- src/Pluf/Exception/DoesNotExist.php
+++ src/Pluf/Exception/DoesNotExist.php
@@ -1,21 +1,19 @@
 <?php
 
 /**
- * خطای عدم وجود منبع
+ * Default not found exception
  * 
- * @author maso
+ * @author webpich.com
  *
  */
 class Pluf_Exception_DoesNotExist extends Pluf_Exception
 {
 
-    public function __construct ($message = null, $previous = null, $link = null, 
-            $developerMessage = null)
+    public function __construct($message = null, $previous = null, $link = null, $developerMessage = null)
     {
         if (! isset($message) || is_null($message)) {
-            $message = __('Resource does not exist.');
+            $message = 'Resource does not exist.';
         }
-        parent::__construct($message, 4102, $previous, 404, $link, 
-                $developerMessage);
+        parent::__construct($message, 4102, $previous, 404, $link, $developerMessage);
     }
 }
