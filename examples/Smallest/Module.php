ff54f56b8fd5d7c0164e19c94edfbccf3098713b
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-03-06 15:20:41 +0330

    some simple applications are added into the project

---
+++ examples/Smallest/Module.php
@@ -0,0 +1,12 @@
+<?php
+namespace Pluf\Smallest;
+
+class Module extends \Pluf_Module
+{
+
+    const moduleJsonPath = __DIR__ . '/module.json';
+
+    const relations = array();
+
+    const urlsPath = __DIR__ . '/urls.php';
+}
