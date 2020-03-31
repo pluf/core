d0fcb100d534dcb22c2481a259064da98150f341
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2020-03-31 18:39:55 +0430

    change test configs to pass tests on mysql database

--- tests/conf/mysql.conf.php
+++ tests/conf/mysql.conf.php
@@ -1,7 +1,8 @@
 <?php
 $cfg = array();
 
-$cfg['db_engine'] = 'MySQL';
+$cfg['db_engine'] = '\Pluf\Db\MySQLEngine';
+$cfg['db_schema'] = '\Pluf\Db\MySQLSchema';
 $cfg['db_version'] = '5.4.1';
 
 $cfg['db_login'] = 'root';
@@ -9,7 +10,7 @@ $cfg['db_password'] = '';
 $cfg['db_server'] = 'localhost';
 $cfg['db_database'] = 'test';
 
-$cfg['db_table_prefix'] = 'pluf_mt_unit_tests_'; 
+$cfg['db_schema_table_prefix'] = 'pluf_core_unit_tests_' . rand() . '_'; 
 
 return $cfg;
 
