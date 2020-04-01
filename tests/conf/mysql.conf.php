85020b83c6d24bbcacc0a4dce878934cc321852d
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2020-04-01 11:59:01 +0430

    fix bug

--- tests/conf/mysql.conf.php
+++ tests/conf/mysql.conf.php
@@ -5,12 +5,12 @@ $cfg['db_engine'] = '\Pluf\Db\MySQLEngine';
 $cfg['db_schema'] = '\Pluf\Db\MySQLSchema';
 $cfg['db_version'] = '5.4.1';
 
-$cfg['db_login'] = 'root';
-$cfg['db_password'] = '';
-$cfg['db_server'] = 'localhost';
-$cfg['db_database'] = 'test';
+$cfg['db_login'] = 'pluf';
+$cfg['db_password'] = 'password';
+$cfg['db_server'] = '127.0.0.1';
+$cfg['db_database'] = 'plufdb';
 
-$cfg['db_schema_table_prefix'] = 'pluf_core_unit_tests_' . rand() . '_'; 
+$cfg['db_schema_table_prefix'] = 'core_' . rand() . '_'; 
 
 return $cfg;
 
