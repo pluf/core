d61496789d763e88c32097d8aa7864d6c6a5ca89
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2021-02-20 16:12:17 +0330

    Delete entity:
    See: https://github.com/pluf/lm/issues/4

--- src/Process/Entity/ReadEntity.php
+++ src/Process/Entity/ReadEntity.php
@@ -1,28 +1,32 @@
 <?php
 namespace Pluf\Core\Process\Entity;
 
+use Pluf\Core\Exception;
 use Pluf\Orm\AssertionTrait;
-use Pluf\Orm\EntityQuery;
 use Pluf\Orm\EntityManager;
+use Pluf\Orm\EntityQuery;
+use Throwable;
 
 class ReadEntity
 {
     use AssertionTrait;
-    
+
     private string $class;
+
     private string $name = "itemId";
-    
+
     public function __construct(string $class, string $name = "itemId")
     {
         $this->class = $class;
         $this->name = $name;
     }
-    
+
     /**
      * Store list of cusomers into the repositoer
      *
      *
-     * @param EntityQuery $entityQuery to perform on entities
+     * @param EntityQuery $entityQuery
+     *            to perform on entities
      * @return array of results
      */
     public function __invoke(EntityManager $entityManager, $itemId)
@@ -34,6 +38,20 @@ class ReadEntity
         ]);
         return $result;
     }
-    
+
+    /**
+     * Creates new exception
+     *
+     * @param string $message
+     * @param int $code
+     * @param Throwable $previous
+     * @param array $params
+     * @param array $solutions
+     * @return Throwable
+     */
+    protected function generateException($message = '', ?int $code = null, ?Throwable $previous = null, ?array $params = [], ?array $solutions = []): Throwable
+    {
+        return new Exception($message, $code, $previous, 404, $params, $solutions);
+    }
 }
 
